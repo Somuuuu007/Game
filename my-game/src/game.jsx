c4425030c19ad10c5be76861ed203dd16421b159
Somesh Lingwal <someshlingwal1@gmail.com>
2025-09-30 21:42:22 +0530

    first commit

---
+++ my-game/src/game.jsx
@@ -0,0 +1,142 @@
+import { useEffect } from "react";
+import Phaser from "phaser";
+
+const Game = () => {
+  useEffect(() => {
+    let game;
+
+    const createGame = () => {
+      const config = {
+        type: Phaser.AUTO,
+        width: window.innerWidth,
+        height: window.innerHeight,
+        parent: "phaser-container",
+        physics: {
+          default: "arcade",
+          arcade: {
+            gravity: { y: 300 },
+            debug: false,
+          },
+        },
+        scene: {
+          preload: function () {
+            this.load.spritesheet("monster_idle", "/monster_idle.png", {
+              frameWidth: 32,
+              frameHeight: 32,
+            });
+            this.load.spritesheet("monster_run", "/monster_run.png", {
+              frameWidth: 32,
+              frameHeight: 32,
+            });
+            this.load.spritesheet("monster_jump", "/monster_jump.png", {
+              frameWidth: 32,
+              frameHeight: 32,
+            });
+          },
+          create: function () {
+            // Set background color to remove green line
+            this.cameras.main.setBackgroundColor('#87CEEB');
+
+            // Create invisible ground platform
+            this.platforms = this.physics.add.staticGroup();
+            const ground = this.add.rectangle(window.innerWidth / 2, window.innerHeight - 32, window.innerWidth, 64, 0x000000, 0);
+            this.physics.add.existing(ground, true);
+            this.platforms.add(ground);
+
+            // Create player
+            this.player = this.physics.add.sprite(100, window.innerHeight - 200, "monster_idle");
+            this.player.setScale(3);
+            this.player.setCollideWorldBounds(true);
+            this.player.body.setSize(20, 28, true);
+
+            // Player collides with platforms
+            this.physics.add.collider(this.player, this.platforms);
+
+            // Create animations with proper frame counts
+            this.anims.create({
+              key: "idle",
+              frames: this.anims.generateFrameNumbers("monster_idle", { start: 0, end: 3 }),
+              frameRate: 8,
+              repeat: -1,
+            });
+
+            this.anims.create({
+              key: "run",
+              frames: this.anims.generateFrameNumbers("monster_run", { start: 0, end: 5 }),
+              frameRate: 10,
+              repeat: -1,
+            });
+
+            this.anims.create({
+              key: "jump",
+              frames: this.anims.generateFrameNumbers("monster_jump", { start: 0, end: 7 }),
+              frameRate: 12,
+              repeat: 0,
+            });
+
+            // Start with idle animation
+            this.player.play("idle");
+            this.cursors = this.input.keyboard.createCursorKeys();
+            this.spaceKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+
+            // Track if player is on ground
+            this.isOnGround = false;
+          },
+          update: function () {
+            const speed = 200;
+            const jumpPower = -400;
+
+            // Check if player is on ground
+            this.isOnGround = this.player.body.touching.down || this.player.body.blocked.down;
+
+            // Horizontal movement
+            if (this.cursors.left.isDown) {
+              this.player.setVelocityX(-speed);
+              this.player.setFlipX(true);
+
+              if (this.isOnGround && this.player.anims.currentAnim.key !== "run") {
+                this.player.play("run");
+              }
+            } else if (this.cursors.right.isDown) {
+              this.player.setVelocityX(speed);
+              this.player.setFlipX(false);
+
+              if (this.isOnGround && this.player.anims.currentAnim.key !== "run") {
+                this.player.play("run");
+              }
+            } else {
+              this.player.setVelocityX(0);
+
+              if (this.isOnGround && this.player.anims.currentAnim.key !== "idle") {
+                this.player.play("idle");
+              }
+            }
+
+            // Jumping
+            if ((this.spaceKey.isDown || this.cursors.up.isDown) && this.isOnGround) {
+              this.player.setVelocityY(jumpPower);
+              this.player.play("jump");
+            }
+
+            // If in air and not already playing jump animation
+            if (!this.isOnGround && this.player.anims.currentAnim.key !== "jump") {
+              this.player.play("jump");
+            }
+          },
+        },
+      };
+
+      game = new Phaser.Game(config);
+    };
+
+    createGame();
+
+    return () => {
+      if (game) game.destroy(true);
+    };
+  }, []);
+
+  return <div id="phaser-container"></div>;
+};
+
+export default Game;
