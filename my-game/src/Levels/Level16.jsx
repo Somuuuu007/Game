c4f39b2d57060a9ad17daf7d76dc6f1d1fc0acad
Somesh Lingwal <someshlingwal1@gmail.com>
2025-10-06 12:51:54 +0530

    jump added

--- my-game/src/Levels/Level16.jsx
+++ my-game/src/Levels/Level16.jsx
@@ -22,8 +22,76 @@ export class Level16Scene extends BaseScene {
   }
 
   update() {
-    super.update();
-    
+    // Override with increased jump power for this level
+    if (!this.levelComplete) {
+      const speed = 300;
+      const jumpPower = -500; // Increased jump power for this level
+
+      this.isOnGround = this.player.body.touching.down || this.player.body.blocked.down;
+
+      // Horizontal movement
+      if (this.cursors.left.isDown || this.aKey.isDown) {
+        this.player.setVelocityX(-speed);
+        this.player.setFlipX(true);
+
+        if (this.isOnGround && this.player.anims.currentAnim.key !== "run") {
+          this.player.play("run");
+        }
+      } else if (this.cursors.right.isDown || this.dKey.isDown) {
+        this.player.setVelocityX(speed);
+        this.player.setFlipX(false);
+
+        if (this.isOnGround && this.player.anims.currentAnim.key !== "run") {
+          this.player.play("run");
+        }
+      } else {
+        this.player.setVelocityX(0);
+
+        if (this.isOnGround && this.player.anims.currentAnim.key !== "idle") {
+          this.player.play("idle");
+        }
+      }
+
+      // Jumping with increased power
+      if ((Phaser.Input.Keyboard.JustDown(this.spaceKey) || Phaser.Input.Keyboard.JustDown(this.cursors.up) || Phaser.Input.Keyboard.JustDown(this.wKey)) && this.isOnGround) {
+        this.player.setVelocityY(jumpPower);
+        this.player.play("jump");
+      }
+
+      if (!this.isOnGround && this.player.anims.currentAnim.key !== "jump") {
+        this.player.play("jump");
+      }
+
+      // Door logic
+      const distanceToDoor = Phaser.Math.Distance.Between(
+        this.player.x, this.player.y,
+        this.door.x, this.door.y
+      );
+
+      if (distanceToDoor < 200 && !this.doorOpen) {
+        this.doorOpen = true;
+        this.door.play("door_opening");
+        this.door.once("animationcomplete", () => {
+          this.door.play("door_open");
+        });
+      }
+
+      if (Math.abs(this.player.x - this.door.x) < 10 && distanceToDoor < 40 && this.doorOpen && !this.levelComplete) {
+        this.levelComplete = true;
+        this.player.body.setVelocity(0, 0);
+        this.player.body.setAllowGravity(false);
+        this.player.setFlipX(false);
+        this.player.stop();
+        this.player.play("walkup", true);
+
+        this.player.once("animationcomplete", () => {
+          this.cameras.main.fadeOut(500, 0, 0, 0);
+          this.cameras.main.once("camerafadeoutcomplete", () => {
+            this.onLevelComplete();
+          });
+        });
+      }
+    }
   }
 
   createPlatforms() {
